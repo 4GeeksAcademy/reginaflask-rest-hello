8335fde0eb9db2db47d3d040de51a975b562f6f2
Regina7411 <danielagg71411@gmail.com>
2025-04-18 01:15:15 +0000

    instagram

--- src/models.py
+++ src/models.py
@@ -1,19 +1,75 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+import os
+import sys
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = 'user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
 
+    posts = relationship('Post', backref='author')
+    comments = relationship('Comment', backref='author')
+    followers = relationship('Follower', backref='user')
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "email": self.email
+        }
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(250))
+    caption = db.Column(db.String(500))
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+
+    comments = relationship('Comment', backref='post')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "image_url": self.image_url,
+            "caption": self.caption,
+            "user_id": self.user_id
+        }
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500))
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, ForeignKey('post.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        }
+
+class Follower(db.Model):
+    __tablename__ = 'follower'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_from_id = db.Column(db.Integer, ForeignKey('user.id'))
+    user_to_id = db.Column(db.Integer, ForeignKey('user.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
         }
