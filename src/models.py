5b89541066ea5bca12752b5acb99d1c3a4d0400a
Regina7411 <danielagg71411@gmail.com>
2025-04-20 01:13:31 +0000

    correciones

--- src/models.py
+++ src/models.py
@@ -1,57 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-    posts = relationship("Post", back_populates="user")
-    media = relationship("Media", back_populates="user")
-
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    
+    
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
         }
+    
 
 class Post(db.Model):
+    __tablename__ = "posts"
     id: Mapped[int] = mapped_column(primary_key=True)
-    content: Mapped[str] = mapped_column(String(500), nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    user = relationship("User", back_populates="posts")
-    comments = relationship("Comment", back_populates="post")
-    media = relationship("Media", back_populates="post")
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    Media: Mapped[list["Media"]] = relationship("Media",back_populate = "post")
 
     def serialize(self):
         return {
             "id": self.id,
-            "content": self.content,
             "user_id": self.user_id
         }
 
 class Comment(db.Model):
+    __tablename__ = "comments"
     id: Mapped[int] = mapped_column(primary_key=True)
     content: Mapped[str] = mapped_column(String(500), nullable=False)
-    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
-    post = relationship("Post", back_populates="comments")
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "content": self.content,
+            "author_id": self.author_id,
             "post_id": self.post_id
         }
 
 class Follower(db.Model):
+    __tablename__ = "followers"
     id: Mapped[int] = mapped_column(primary_key=True)
-    follower_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    following_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    follower = relationship("User", foreign_keys=[follower_id], backref="followed_by")
-    following = relationship("User", foreign_keys=[following_id], backref="follows")
+    follower_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    following_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
 
     def serialize(self):
         return {
@@ -61,19 +62,17 @@ class Follower(db.Model):
         }
 
 class Media(db.Model):
+    __tablename__ = "medias"
     id: Mapped[int] = mapped_column(primary_key=True)
     url: Mapped[str] = mapped_column(String(200), nullable=False)
-    media_type: Mapped[str] = mapped_column(String(50), nullable=False)  
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=True)  
-    user = relationship("User", back_populates="media")
-    post = relationship("Post", back_populates="media")
+    type: Mapped[str] = mapped_column(String(50), nullable=False)  
+    post_id: Mapped[int] = mapped_column(ForeignKey('posts.id'), nullable=True)  
+    
 
     def serialize(self):
         return {
             "id": self.id,
             "url": self.url,
-            "media_type": self.media_type,
-            "user_id": self.user_id,
+            "type": self.type,
             "post_id": self.post_id
         }
